63a82693b291cc63bc079490ad23a2815a7a9962
Liam Zoe Stead <liamstead22@gmail.com>
2024-11-13 18:27:55 -0500

    add an attempt

--- stead-zoe-assignment3-2dgame/Game.cs
+++ stead-zoe-assignment3-2dgame/Game.cs
@@ -1,4 +1,5 @@
 ﻿// Include code libraries you need below (use the namespace).
+using Microsoft.VisualBasic;
 using System;
 using System.Numerics;
 
@@ -11,14 +12,36 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        // Variables for the colours of the shapes
+        Vector2 position;
+        float radius = 35;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            // Window set size and title
+            Window.SetTitle("Shape Bolt");
+            Window.SetSize(800, 600);
+        }
 
+        void GetPlayerInput()
+        {
+
+            Vector2 input = Vector2.Zero;
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Up))
+            {
+                input.Y -= 1 + 1;
+            }
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Down))
+            {
+                input.Y += 1 + 1;
+            }
+
+            Input = input;
         }
 
         /// <summary>
@@ -26,7 +49,14 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            // Change background colour to selected background colour
+            Window.ClearBackground(Color.OffWhite);
 
+            position += Input * 100 * Time.DeltaTime;
+
+            Draw.FillColor = Color.Yellow;
+            Draw.LineColor = Color.Black;
+            Draw.Square(105, 105, 75);
         }
     }
 }
